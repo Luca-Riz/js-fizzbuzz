d7512a4d24092e7639f3325cab9d71d3c606a4ec
Luca-Riz <luca983@gmail.com>
2021-06-03 14:37:56 +0200

    add end

--- js/script.js
+++ js/script.js
@@ -7,14 +7,26 @@
 // scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
+var num;
+var arrayNum = [];
 
 // stampare i numeri da 1 a 100
+for(var i=0; i<100; i++){
+    num = i+1;
+    
+    // se multipli di 3 aggiungere Fizz
+    if(num % 3 == 0 && num % 5 != 0){
+        arrayNum.push(num+'Fizz');
+    } // se multipli di 5 aggiungere Buzz
+    else if(num % 5 == 0 && num % 3 != 0){
+        arrayNum.push(num+'Buzz');
+    } // se divisibile sia per e che per 5
+    else if(num % 3 == 0 && num % 5 == 0){
+        arrayNum.push(num+'FizzBuzz');
+    } else {
+        arrayNum.push(num);
+    }
+}
 
+console.log(arrayNum);
 
-
-// se multipli di 3 aggiungere Fizz
-
-
-
-
-// se multipli di 5 aggiungere Buzz
